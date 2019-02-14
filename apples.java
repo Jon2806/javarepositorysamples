f6df17a8abdf43c71725162251716419fd783f1d
Jon2806 <jbb130131@gmail.com>
2019-02-14 22:12:38 +0800

    sample java

---
+++ apples.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class apples{
+	public static void main(String args []) {
+	 Scanner bucky = new Scanner(System.in);
+	 int a, b, c;
+	 
+	 a= 6;
+	 b= 3;
+	 c= a +b;
+	 
+	 	System.out.print(c);
+	 	
+		
+	}
+	
+	
+	
+}
